b3d47efb8a58d8b4f9d3d461a4479623026de771
Cesar Ibarra <cesarij07@gmail.com>
2023-12-14 17:26:23 -0600

    chore(hello world): first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,10 @@
+void main(){
+  // var myName = "César";
+  // late final myName;
+  // String myName = "César";
+  const myName = "César";
+
+  print("Hola $myName");
+  print("Hola ${ myName.toUpperCase() }");
+  print("Hola ${ 1+1 }");
+}
